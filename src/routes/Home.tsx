1d31300e268adc7cc68927de50317965f485cc21
a0666586 <dusan.pausly.ext@dna.fi>
2022-04-03 13:01:57 +0200

    initial commit

---
+++ src/routes/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "@gooddata/sdk-ui-filters/styles/css/main.css";
+
+import Page from "../components/Page";
+
+const Home: React.FC = () => {
+
+    return (
+        <Page>
+        </Page>
+    );
+};
+
+export default Home;
